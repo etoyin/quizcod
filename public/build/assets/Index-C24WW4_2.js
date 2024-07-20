0dc243d2771f3b99723db563faf1ef886ed7b3c5
etoyin <toyinadesina60@gmail.com>
2024-07-20 08:16:27 +0100

    build

---
+++ public/build/assets/Index-C24WW4_2.js
@@ -0,0 +1,17 @@
+import{r as s,j as e,a as t}from"./app-BFa-IAZl.js";import"./Dropdown-yAf727HP.js";import"./Chirp-BR8E3NBk.js";import"./transition-DPZ7cm77.js";import"./InputError-BLIi1pqW.js";import"./PrimaryButton-CwTmQvPk.js";function m({auth:o,chirps:i}){const[a,n]=s.useState(!1),r=()=>{n(!0)};return e.jsx("div",{children:e.jsxs("div",{className:"bg-dark-blue fixed left-0 top-0 h-full w-full h-screen text-white",children:[e.jsx("h2",{className:"text-3xl uppercase text-center p-5",children:"Tabiths's Conference Genius Show"}),e.jsxs("div",{className:"flex items-center mt-10 m-auto h-96 justify-center p-5",children:[e.jsx(t,{href:"/question/cat1",children:e.jsx("div",{onClick:r,className:`shadow-md shadow-silver\r
+                                rounded-lg h-64\r
+                                border\r
+                                text-center \r
+                                text-white text-bold \r
+                                hover:bg-blue\r
+                                hover:text-midnight\r
+                                hover:border-white\r
+                                uppercase cursor-pointer text-2xl content-center w-36 mr-10 bg-dark-blue `,children:"Cat 1"})}),e.jsx(t,{href:"/question/cat2",children:e.jsx("div",{onClick:r,className:`shadow-md shadow-silver\r
+                                rounded-lg h-64\r
+                                border\r
+                                text-center \r
+                                text-white text-bold \r
+                                hover:bg-blue\r
+                                hover:text-midnight\r
+                                hover:border-white\r
+                                uppercase cursor-pointer text-2xl content-center w-36 mr-10 bg-dark-blue `,children:"Cat 2"})})]})]})})}export{m as default};
---
+++ public/build/assets/Level-CvlI0Svp.js
@@ -0,0 +1,1 @@
+import{j as e}from"./app-BFa-IAZl.js";function o({results:s,close:t}){return e.jsx("div",{})}export{o as default};
